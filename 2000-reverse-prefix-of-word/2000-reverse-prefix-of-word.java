795def7117cfe338897454d032ad654b9189ca8a
Omkar09 <op8686627@gmail.com>
2026-09-02 20:36:20 +0530

    Time: 1 ms (46.6%), Space: 42.7 MB (96.82%) - LeetHub

---
+++ 2000-reverse-prefix-of-word/2000-reverse-prefix-of-word.java
@@ -0,0 +1,10 @@
+class Solution {
+    public String reversePrefix(String word, char ch) {
+        int index = word.indexOf(ch);
+
+        StringBuilder sb = new StringBuilder(word.substring(0, index + 1));
+        sb.reverse();
+
+        return sb + word.substring(index + 1);
+    }
+}
